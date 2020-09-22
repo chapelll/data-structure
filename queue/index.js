19f700a6bf508f1e6c3382d6bb63b429fd1a5044
chapelll <214426857@qq.com>
2020-09-22 11:45:29 +0800

    队列

---
+++ queue/index.js
@@ -0,0 +1,10 @@
+let queue = []
+
+queue.push(1)
+queue.push(2)
+
+let item1 = queue.shift() 
+//移除数组第一个元素并返回这个元素
+let item2 = queue.shift() 
+
+// 队列: 先进先出，后进后出的数据结构
--- stack栈/index.js
+++ stack栈/index.js
@@ -5,3 +5,5 @@ stack.push(2)
 
 let item1 = stack.pop()
 let item2 = stack.pop()
+
+// 栈: 先进后出，后进先出
